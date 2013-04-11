86a29b853406228cba925146dae56c4c2420e2b6
trDna <trdna@live.ca>
2013-04-10 23:44:53 -0400

    Added some changes (0.1)

--- src/org/javahacking/jhbl/io/JarWriter.java
+++ src/org/javahacking/jhbl/io/JarWriter.java
@@ -1,28 +1,27 @@
 package org.javahacking.jhbl.io;
 
-import org.javahacking.jhbl.StoragePool;
 import org.objectweb.asm.tree.ClassNode;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.util.HashMap;
 import java.util.jar.JarEntry;
 import java.util.jar.JarOutputStream;
 
 /**
- * Created with IntelliJ IDEA.
- * User: Tim
- * Date: 1/28/13
- * Time: 4:17 AM
- * To change this template use File | Settings | File Templates.
+ * Obvious stuff.
+ *
+ * @author Swipe
+ * @author trDna
  */
 public class JarWriter {
 
-    public JarWriter(String location) {
+    public JarWriter(String location, HashMap<String, ClassNode> clazzez) {
         try {
             File file = new File(location);
             FileOutputStream stream = new FileOutputStream(file);
             JarOutputStream out = new JarOutputStream(stream);
-            for (ClassNode classNode : StoragePool.getClasses().values()) {
+            for (ClassNode classNode : clazzez.values()) {
                 JarEntry je = new JarEntry(classNode.name.replace('.', '/') + ".class");
                 out.putNextEntry(je);
                 out.write(classNode.getBytes(false));
